65b0b21e8a07d492fe209307211a570e798e0df9
Robert Newson <robert.newson@gmail.com>
2010-02-10 23:05:00 +0000

    wip

--- src/main/java/com/github/rnewson/couchdb/lucene/rest/Search.java
+++ src/main/java/com/github/rnewson/couchdb/lucene/rest/Search.java
@@ -8,16 +8,17 @@ import java.util.List;
 
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 
-@Path("/")
+@Path("/{db}/{ddoc}/{view}")
 public class Search {
 
     @GET
-    @Path("/{db}/{ddoc}/{view}")
+    @Path("search")
     @Produces({"text/plain", "application/json"})
     public String search(
             @PathParam("db") final String databaseName,
@@ -28,10 +29,26 @@ public class Search {
             @DefaultValue("25") @QueryParam("limit") final int limit,
             @QueryParam("sort") final List<String> sort,
             @DefaultValue("false") @QueryParam("include_docs") final boolean includeDocs) {
-        System.err.println(databaseName);
-        System.err.println(designDocumentName);
-        System.err.println(viewName);
-        System.err.println(sort);
-        return "hello";
+        return query;
     }
+
+    @GET
+    @Produces({"text/plain", "application/json"})
+    public String info(
+            @PathParam("db") final String databaseName,
+            @PathParam("ddoc") final String designDocumentName,
+            @PathParam("view") final String viewName) {
+        return viewName;
+    }
+
+    @PUT
+    @Path("_optimize")
+    @Produces({"text/plain", "application/json"})
+    public String optimize(
+            @PathParam("db") final String databaseName,
+            @PathParam("ddoc") final String designDocumentName,
+            @PathParam("view") final String viewName) {
+        return "ok";
+    }
+
 }
